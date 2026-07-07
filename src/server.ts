c57265f0716bc85976310ff58a1312f805b42c4e
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-07-07 15:19:32 +0600

    database connected and prisma connected

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
 import dotenv from "dotenv";
+import config from "./config";
+import { prisma } from "./config/prisma";
+
+
 dotenv.config();
 
 import app from "./app";
@@ -6,12 +10,15 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
+    await prisma.$connect();
+    console.log("Database Connected");
     app.listen(PORT, () => {
       console.log(`Server running on http://localhost:${PORT}`);
     });
-  } catch (error) {
-    console.error("Failed to start server:", error);
-    process.exit(1);
+  } catch (err) {
+    console.error("error starting server:", err);
+    await prisma.$disconnect();
+    process.exit(1)
   }
 };
 
