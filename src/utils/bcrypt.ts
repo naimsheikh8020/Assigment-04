d209dfa4fede23d2378545d4f35064eb3f098686
naimsheikh8020 <naimsheikh8020@gmail.com>
2026-07-09 18:39:55 +0600

    refactor bcrypt utility functions for improved readability

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -2,18 +2,12 @@ import bcrypt from "bcryptjs";
 import config from "../config";
 
 export const hashPassword = async (password: string): Promise<string> => {
-  return await bcrypt.hash(
-    password,
-    Number(config.bycrypt_salt_rounds)
-  );
+  return await bcrypt.hash(password, Number(config.bcrypt_salt_rounds));
 };
 
 export const comparePassword = async (
   plainPassword: string,
-  hashedPassword: string
+  hashedPassword: string,
 ): Promise<boolean> => {
-  return await bcrypt.compare(
-    plainPassword,
-    hashedPassword
-  );
+  return await bcrypt.compare(plainPassword, hashedPassword);
 };
